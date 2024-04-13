02a0943eb2f8728db9d6b466c2da91c1811371cc
Bryan Cave <bryan.d.cave@gmail.com>
2024-04-13 18:59:59 -0300

    adding basic navigation

--- pathfinder/urls.py
+++ pathfinder/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 urlpatterns = [
     # ex: /polls/loot/5/
+    path("loot", views.loot),
     path("loot/<int:player_level>/", views.loot, name="loot"),
     path("encounter/<int:id>/", views.encounter, name="encounter"),
+    path("session/", views.session),
     path("session/<int:id>/", views.session, name="session")
 ]
--- pathfinder/views.py
+++ pathfinder/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.conf import settings
+from datetime import datetime as dt
 
 from .models import Loot, GoldTracker, Encounter, EncounterViewModel, SessionViewModel, Session
 
 # Create your views here.
-def loot(request, player_level):
+def loot(request, player_level=settings.PARTY_LEVEL):
     perms = Loot.objects.filter(rewarded_level=player_level, consumable=False)
     consumables = Loot.objects.filter(rewarded_level=player_level, consumable=True)
     gold = GoldTracker.objects.filter(level=player_level)
@@ -28,8 +30,15 @@ def encounter(request, id):
 
     return HttpResponse(template.render(context, request))
 
-def session(request, id):
-    session = Session.objects.get(id=id)
+def session(request, id=None):
+    if id is None:
+        sessions = Session.objects.all()
+        for sess in sessions:
+            session = sess
+            if sess.date >= dt.now().date():
+                break
+    else:
+        session = Session.objects.get(id=id)
     template = loader.get_template("pathfinder/session.html")
     viewModel = SessionViewModel(session)
     context = {
